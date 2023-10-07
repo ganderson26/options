99bc8a8496fbf36aee7a00e78df13931a4dc7a4f
ganderson <ganderson@26miles.com>
2023-10-07 11:07:10 -0500

    chatgpt examples

---
+++ src/stock-direction.py
@@ -0,0 +1,65 @@
+# Import necessary libraries
+import yfinance as yf
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
+# Function to get historical stock data
+def get_stock_data(ticker, start_date, end_date):
+    data = yf.download(ticker, start=start_date, end=end_date)
+    return data
+
+# Function to prepare the data for training and testing
+def prepare_data(data, window_size=5):
+    data['Price_Up'] = data['Close'].shift(-window_size) > data['Close']
+    X = data[['Open', 'High', 'Low', 'Close', 'Volume']].values
+    y = data['Price_Up'].values
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    return X[:-window_size], y[:-window_size]
+
+# Function to train the SVM model
+def train_svm(X_train, y_train):
+    svm_classifier = SVC(kernel='linear')
+    svm_classifier.fit(X_train, y_train)
+    return svm_classifier
+
+# Main function
+def main():
+    # Define the stock and date range
+    ticker = 'ORCL'  
+    start_date = '2023-01-01'
+    end_date = '2023-10-13'
+
+    # Get historical stock data
+    stock_data = get_stock_data(ticker, start_date, end_date)
+
+    # Prepare the data
+    X, y = prepare_data(stock_data)
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Train the SVM model
+    svm_model = train_svm(X_train, y_train)
+
+    # Make predictions on the test data
+    predictions = svm_model.predict(X_test)
+
+    # Calculate accuracy
+    accuracy = accuracy_score(y_test, predictions)
+    print(f'Accuracy: {accuracy * 100:.2f}%')
+
+    # Predict the next few days
+    last_data = X[-1].reshape(1, -1)
+    prediction = svm_model.predict(last_data)
+    
+    if prediction[0]:
+        print('Predicted price for ' + ticker + ' will go up by ' + end_date)
+    else:
+        print('Predicted price for ' + ticker + ' will go down by ' + end_date)
+
+if __name__ == "__main__":
+    main()
---
+++ src/stock-price.py
@@ -0,0 +1,66 @@
+# Install Python
+#https://www.python.org/downloads/
+
+# Install packages from command line
+#pip install pandas scikit-learn yfinance
+
+
+# Importing necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import yfinance as yf
+
+# Function to get historical stock price data
+def get_stock_data(ticker, start_date, end_date):
+    data = yf.download(ticker, start=start_date, end=end_date)
+    return data
+
+# Function to preprocess data and train the model
+def predict_stock_price(ticker, start_date, end_date, prediction_days):
+    # Getting historical stock data
+    stock_data = get_stock_data(ticker, start_date, end_date)
+    
+    # Feature engineering: using previous 'n' days' closing prices as features
+    for i in range(1, prediction_days+1):
+        stock_data[f'Close_Lag_{i}'] = stock_data['Close'].shift(i)
+    
+    # Dropping rows with NaN values
+    stock_data.dropna(inplace=True)
+    
+    # Splitting data into features and target variable
+    X = stock_data.drop(['Close'], axis=1)
+    y = stock_data['Close']
+    
+    # Splitting data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    # Initializing and training the linear regression model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    
+    # Predicting stock prices for the next 'prediction_days' days
+    last_days_data = X[-prediction_days:]
+    predicted_prices = model.predict(last_days_data)
+    
+    return predicted_prices
+
+# Example usage
+if __name__ == "__main__":
+    # Ticker symbol of the stock you want to predict
+    ticker = "ORCL"
+    # Date range for historical data (format: "YYYY-MM-DD")
+    start_date = "2023-01-01"
+    end_date = "2023-10-09"
+    # Number of days to predict into the future
+    prediction_days = 5
+    
+    # Predict stock prices
+    predicted_prices = predict_stock_price(ticker, start_date, end_date, prediction_days)
+    
+    # Print the predicted prices for the next 'prediction_days' days
+    print(ticker + f" Prediction for the next {prediction_days} days starting " + end_date)
+
+    for i in range(prediction_days):
+        print(f"Predicted price for Day {i+1}: {predicted_prices[i]}")
